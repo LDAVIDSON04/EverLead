7cc6e526eafcf19a884c12c5c4c48f2a83d9510b
Liam Davidson <liamdavidson@Liams-MacBook-Air.local>
2025-12-17 13:00:04 -0600

    Fix JSX fragment closing tag in search page modal

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -593,7 +593,7 @@ function SearchResults() {
               )}
             </div>
           </div>
-        </div>
+        </>
       )}
       {/* Header */}
       <header className="bg-white border-b border-gray-200 sticky top-0 z-50">
