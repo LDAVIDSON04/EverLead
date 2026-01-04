753ee3d3131ce107e30f77bf825b0c062cc8d753
Liam Davidson <liamdavidson@Liams-MacBook-Air.local>
2026-01-04 10:43:32 -0800

    Remove black TrustedBySection from learn-more-about-starting page

--- src/app/learn-more-about-starting/page.tsx
+++ src/app/learn-more-about-starting/page.tsx
@@ -14,7 +14,6 @@ export default function LearnMoreAboutStartingPage() {
       <Header />
       <main>
         <HeroSection />
-        <TrustedBySection />
         <SolutionsSection />
         <CaseStudiesSection />
         <IntegrationSection />
